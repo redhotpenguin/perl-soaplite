ce2c360ae93c8e489f4f7424c8559a74a642b3b8
Martin Kutter <martin.kutter@fen-net.de>
2007-11-15 08:44:32 +0000

    - fixed #16897 SOAP::Lite 0.66 fails with UNIVERSAL::use

--- lib/SOAP/Lite.pm
+++ lib/SOAP/Lite.pm
@@ -2860,26 +2860,27 @@ use vars qw(@ISA $AUTOLOAD);
 use overload fallback => 1, '""' => sub { shift->value };
 
 sub _compileit {
-  no strict 'refs';
-  my $method = shift;
-  *$method = sub { 
-    return __PACKAGE__->SUPER::name($method => $_[0]->attr->{$method})
-      if exists $_[0]->attr->{$method};
-    my @elems = grep {
-      ref $_ && UNIVERSAL::isa($_ => __PACKAGE__) && $_->SUPER::name =~ /(^|:)$method$/
-    } $_[0]->value;
-    return wantarray? @elems : $elems[0];
-  }
+    no strict 'refs';
+    my $method = shift;
+    *$method = sub { 
+        return __PACKAGE__->SUPER::name($method => $_[0]->attr->{$method})
+            if exists $_[0]->attr->{$method};
+        my @elems = grep {
+            ref $_ && UNIVERSAL::isa($_ => __PACKAGE__) 
+            && $_->SUPER::name =~ /(^|:)$method$/
+        } $_[0]->value;
+        return wantarray? @elems : $elems[0];
+    };
 }
 
-sub BEGIN { foreach (qw(name type import)) { _compileit($_) } }
+sub BEGIN { foreach (qw(name type import use)) { _compileit($_) } }
 
 sub AUTOLOAD {
-  my $method = substr($AUTOLOAD, rindex($AUTOLOAD, '::') + 2);
-  return if $method eq 'DESTROY';
+    my $method = substr($AUTOLOAD, rindex($AUTOLOAD, '::') + 2);
+    return if $method eq 'DESTROY';
 
-  _compileit($method);
-  goto &$AUTOLOAD;
+    _compileit($method);
+    goto &$AUTOLOAD;
 }
 
 # ======================================================================
