b4d59af9e31b9c56b1ffda2f1aa288bc05218e24
Martin Kutter <martin.kutter@fen-net.de>
2007-11-17 22:53:49 +0000

    - bumped up version - updated Changes

--- lib/SOAP/Lite.pm
+++ lib/SOAP/Lite.pm
@@ -21,7 +21,7 @@ package SOAP::Lite;
 use 5.004;
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.70_02';
+$VERSION = '0.70_03';
 
 # ======================================================================
 
