043a4548aab2fe687dc28014430e19787c16c0a7
Martin Kutter <martin.kutter@fen-net.de>
2008-02-28 21:27:36 +0000

    - bumped up version - updated readme

--- lib/SOAP/Lite.pm
+++ lib/SOAP/Lite.pm
@@ -19,7 +19,7 @@ package SOAP::Lite;
 use 5.005;
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.70_08';
+$VERSION = '0.71';
 
 # ======================================================================
 
