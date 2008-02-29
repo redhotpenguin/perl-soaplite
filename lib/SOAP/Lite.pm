a706ec21bdbd7d8856093dda5e2a3549591de99d
Martin Kutter <martin.kutter@fen-net.de>
2008-02-29 21:01:35 +0000

    - updated Changes - update version number

--- lib/SOAP/Lite.pm
+++ lib/SOAP/Lite.pm
@@ -19,7 +19,7 @@ package SOAP::Lite;
 use 5.005;
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.71';
+$VERSION = '0.71.01';
 
 # ======================================================================
 
