6cff8d2af0461cb9d0daa4a29019e2f357644eed
Robbie Bow <soaplite-dev@lists.sourceforge.net>
2008-08-25 15:52:46 +0000

    Very bare start of the Manual

---
+++ lib/SOAP/Lite/Manual.pm
@@ -0,0 +1,23 @@
+=head1 NAME
+
+SOAP::Lite::Manual - User guide and reference for SOAP::Lite
+
+=head1 DESCRIPTION
+
+This is the (table of contents page of the) user guide and
+reference for SOAP::Lite.
+
+=head1 CONTENTS
+
+=item *
+
+L<SOAP::Lite::Manual::Cookbook>
+
+A selection of code for achieving things with SOAP::Lite.
+
+=head1 COPYRIGHT
+
+This program is free software, you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
